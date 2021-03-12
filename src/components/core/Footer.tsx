0d7bce583a48ded69960a6a5995bc8cd5b696fee
Damien Angelos <bostaunieux@gmail.com>
2021-03-11 20:49:49 -0500

    adjust svg logo color based on theme

--- src/components/core/Footer.tsx
+++ src/components/core/Footer.tsx
@@ -1,12 +1,18 @@
+import React from "react";
+import { ToggleThemeContext } from "components/theme/ThemeProvider";
+import NextJsLogo from "components/icons/nextjs.svg";
 import styles from "./Footer.module.css";
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    Built by Damien Angelos using
-    <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
-      <img src="/nextjs.svg" alt="Next.js" className={styles.logo} />
-    </a>
-  </footer>
-);
+const Footer = () => {
+  const { isDark } = React.useContext(ToggleThemeContext);
+  return (
+    <footer className={styles.footer}>
+      Built by Damien Angelos using
+      <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
+        <NextJsLogo style={{ fill: isDark ? "#fff" : "#000" }} />
+      </a>
+    </footer>
+  );
+};
 
 export default Footer;
