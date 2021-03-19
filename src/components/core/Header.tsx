1fe703da51570fa219379eb0db34daf20b0bc7c1
Damien Angelos <bostaunieux@gmail.com>
2021-03-18 20:30:38 -0400

    update logo

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -1,18 +1,23 @@
+import React from "react";
 import NextLink from "next/link";
 import { AppBar, Button, Link as MuiLink, Toolbar } from "@material-ui/core";
 import { LinkedIn as LinkedInIcon, GitHub as GitHubIcon } from "@material-ui/icons";
+import { ToggleThemeContext } from "components/theme/ThemeProvider";
+import SiteLogo from "components/icons/dangelos-dev.svg";
 import ThemeToggle from "components/core/ThemeToggle";
 import UtilitiesLinks from "./UtilitiesLinks";
 import routes from "./routes";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const { isDark } = React.useContext(ToggleThemeContext);
+
   return (
     <AppBar position="static" className={styles.container}>
       <Toolbar>
         <NextLink href={routes.HOME} passHref>
           <Button component="a" color="inherit">
-            dangelos.dev
+            <SiteLogo className={styles.logo} />
           </Button>
         </NextLink>
 
