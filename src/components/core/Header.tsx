5bb749d936114ea4d41fbc467ef342366717d98c
Damien Angelos <bostaunieux@gmail.com>
2021-03-22 20:59:28 -0400

    update theme

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import NextLink from "next/link";
-import { AppBar, Button, Link as MuiLink, Toolbar } from "@material-ui/core";
+import { AppBar, Button, Hidden, Link as MuiLink, Toolbar } from "@material-ui/core";
 import { LinkedIn as LinkedInIcon, GitHub as GitHubIcon } from "@material-ui/icons";
 import SiteLogo from "components/icons/dangelos-dev.svg";
 import ThemeToggle from "components/core/ThemeToggle";
+import { ToggleThemeContext } from "components/theme/ThemeProvider";
 import UtilitiesLinks from "./UtilitiesLinks";
 import routes from "./routes";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const { isDark } = React.useContext(ToggleThemeContext);
   return (
     <AppBar position="static" className={styles.container}>
       <Toolbar>
         <NextLink href={routes.HOME} passHref>
           <Button component="a" color="inherit">
-            <SiteLogo className={styles.logo} />
+            <SiteLogo className={styles.logo} style={{ fill: isDark ? "#fff" : "#222" }} />
           </Button>
         </NextLink>
 
@@ -24,12 +26,12 @@ const Header = () => {
         <div className={styles.externalLinks}>
           <NextLink href="https://github.com/bostaunieux" passHref>
             <MuiLink component="a" color="inherit" className={styles.link}>
-              <GitHubIcon fontSize="small" /> GitHub
+              <GitHubIcon fontSize="small" /> <Hidden only="xs">GitHub</Hidden>
             </MuiLink>
           </NextLink>
           <NextLink href="https://www.linkedin.com/in/damienangelos" passHref>
             <MuiLink component="a" color="inherit" className={styles.link}>
-              <LinkedInIcon fontSize="small" /> LinkedIn
+              <LinkedInIcon fontSize="small" /> <Hidden only="xs">LinkedIn</Hidden>
             </MuiLink>
           </NextLink>
           <ThemeToggle />
--- src/components/theme/theme.ts
+++ src/components/theme/theme.ts
@@ -2,11 +2,22 @@ import { createMuiTheme, ThemeOptions } from "@material-ui/core";
 import blue from "@material-ui/core/colors/blue";
 
 export const lightTheme = createMuiTheme({
+  overrides: {
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: "#fff",
+      },
+    },
+    MuiToolbar: {
+      root: {
+        color: "#353535",
+      },
+    },
+  },
   palette: {
     type: "light",
     primary: {
-      // main: "#6886c5",
-      main: "#1976d2",
+      main: "#2e73ab",
     },
     secondary: {
       main: "#ffe0ac",
@@ -15,7 +26,7 @@ export const lightTheme = createMuiTheme({
       main: "#E44C65",
     },
     background: {
-      default: "#f9f9f9",
+      default: "#F9F9FA",
     },
     text: {
       primary: "#353535",
@@ -24,27 +35,38 @@ export const lightTheme = createMuiTheme({
 });
 
 export const darkTheme = createMuiTheme({
+  overrides: {
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: "#323233",
+      },
+    },
+    MuiToolbar: {
+      root: {
+        color: "#fff",
+      },
+    },
+  },
   palette: {
     type: "dark",
-    // primary: blue,
     primary: {
       // ...blue,
-      main: "#333",
+      main: "#3C3C3C",
       contrastText: "#fff",
       //   // main: "#0f4c75",
     },
     secondary: {
-      main: "#3282b8",
+      main: "#2B2A2D",
     },
     error: {
       main: "#E44C65",
     },
     background: {
-      default: "#212121",
+      default: "#2B2A2D",
       // default: "#1b262c",
     },
     text: {
-      primary: "#bbdefb",
+      primary: "#F9F9FA",
       // primary: "#90caf9",
 
       // primary: "#bbe1fa",
